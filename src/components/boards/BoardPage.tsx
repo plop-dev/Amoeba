1df3d2b362e5c05e762a04eda1a8e5ff18284dd0
plop <plop.dev0@gmail.com>
2025-01-15 00:54:53 +0000

    added minimal tiptap editor credit: Aslam97

--- src/components/boards/BoardPage.tsx
+++ src/components/boards/BoardPage.tsx
@@ -1,1 +1,24 @@
-export function BoardPage() {}
+import { useState } from 'react';
+import { type Content } from '@tiptap/react';
+import { MinimalTiptapEditor } from '@/components/minimal-tiptap';
+import { TooltipProvider } from '@/components/ui/tooltip';
+
+export function BoardPage() {
+	const [value, setValue] = useState<Content>('');
+
+	return (
+		<TooltipProvider>
+			<MinimalTiptapEditor
+				value={value}
+				onChange={setValue}
+				className='w-full'
+				editorContentClassName='p-5'
+				output='html'
+				placeholder='Type your description here...'
+				autofocus={true}
+				editable={true}
+				editorClassName='focus:outline-none'
+			/>
+		</TooltipProvider>
+	);
+}
