6a24cdac7b91b3b23770fa2104c967c10cf9c0e6
plop <plop.dev0@gmail.com>
2025-01-15 22:46:07 +0000

    add loading indicator to ChatPage and enhance minimal-tiptap editor styling; remove unused code from chat page

--- src/components/Chat/ChatPage.tsx
+++ src/components/Chat/ChatPage.tsx
@@ -1,9 +1,12 @@
 import { ChatContainer } from '@/components/Chat/ChatContainer';
 import { ChatInput } from '@/components/Chat/ChatInput';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { LoaderCircle } from 'lucide-react';
 
 export function ChatPage() {
 	const [replyTo, setReplyTo] = useState<string>('');
+	const [loading, setLoading] = useState(true);
+	const [isVisible, setIsVisible] = useState(true);
 
 	const handleReply = (messageId: string) => {
 		const messageElement = document.querySelector(`[data-message-id="${messageId}"]`);
@@ -18,10 +21,34 @@ export function ChatPage() {
 		}
 	};
 
+	useEffect(() => {
+		// Simulate loading time
+		const timer = setTimeout(() => {
+			setLoading(false);
+			setTimeout(() => {
+				setIsVisible(false);
+			}, 300); // Duration of the fade-out transition
+		}, 2000); // Adjust the time as needed
+
+		return () => clearTimeout(timer);
+	}, []);
+
 	return (
 		<div className='container grid grid-cols-[auto] grid-rows-[24fr_1fr] max-h-[calc(100vh-4rem-2rem)] w-full gap-y-4'>
 			<ChatContainer handleReply={handleReply} />
 			<ChatInput replyTo={replyTo} setReplyTo={setReplyTo} />
+
+			{isVisible && (
+				<div
+					className={`absolute inset-0 flex items-center justify-center bg-background z-50 transition-opacity duration-300 ${
+						loading ? 'opacity-100' : 'opacity-0'
+					}`}>
+					<div className='flex flex-col items-center gap-4'>
+						<LoaderCircle className='animate-spin'></LoaderCircle>
+						<p className='text-sm font-medium'>Loading Chats...</p>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/components/minimal-tiptap/minimal-tiptap.tsx
+++ src/components/minimal-tiptap/minimal-tiptap.tsx
@@ -63,7 +63,7 @@ export const MinimalTiptapEditor = React.forwardRef<HTMLDivElement, MinimalTipta
 				as='div'
 				name='editor'
 				ref={ref}
-				className={cn('flex h-auto min-h-72 w-full flex-col rounded-md border border-input shadow-sm ', className)}>
+				className={cn('flex h-auto min-h-72 w-full flex-col rounded-md border border-input shadow-sm focus-within:border-primary/75', className)}>
 				<Toolbar editor={editor} />
 				<EditorContent editor={editor} className={cn('minimal-tiptap-editor', editorContentClassName)} />
 				<LinkBubbleMenu editor={editor} />
