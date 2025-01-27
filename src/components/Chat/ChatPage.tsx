d3a9b38452fd4f61ec4455b9defc0ce2943a7a20
plop <plop.dev0@gmail.com>
2025-01-27 17:30:42 +0000

    fixed hooks issue with toaster

--- src/components/Chat/ChatPage.tsx
+++ src/components/Chat/ChatPage.tsx
@@ -2,6 +2,7 @@ import { ChatContainer } from '@/components/Chat/ChatContainer';
 import { ChatInput } from '@/components/Chat/ChatInput';
 import { useState, useEffect } from 'react';
 import { LoaderCircle } from 'lucide-react';
+import { Toaster } from '@/components/ui/toaster';
 
 export function ChatPage() {
 	const [loading, setLoading] = useState(true);
@@ -35,21 +36,24 @@ export function ChatPage() {
 	}, []);
 
 	return (
-		<div className='container grid grid-cols-[auto] grid-rows-[24fr_1fr] max-h-[calc(100vh-4rem-2rem)] gap-y-4 max-w-full'>
-			<ChatContainer replyingTo={replyingTo} onReplyClick={handleReplyClick} />
-			<ChatInput replyingTo={replyingTo} onClearReply={() => setReplyingTo(null)} />
+		<>
+			<Toaster></Toaster>
+			<div className='container grid grid-cols-[auto] grid-rows-[24fr_1fr] max-h-[calc(100vh-4rem-2rem)] gap-y-4 max-w-full'>
+				<ChatContainer replyingTo={replyingTo} onReplyClick={handleReplyClick} />
+				<ChatInput replyingTo={replyingTo} onClearReply={() => setReplyingTo(null)} />
 
-			{isVisible && (
-				<div
-					className={`absolute inset-0 flex items-center justify-center bg-background z-50 transition-opacity duration-300 ${
-						loading ? 'opacity-100' : 'opacity-0'
-					}`}>
-					<div className='flex flex-col items-center gap-4'>
-						<LoaderCircle className='animate-spin'></LoaderCircle>
-						<p className='text-sm font-medium'>Loading Chats...</p>
+				{isVisible && (
+					<div
+						className={`absolute inset-0 flex items-center justify-center bg-background z-50 transition-opacity duration-300 ${
+							loading ? 'opacity-100' : 'opacity-0'
+						}`}>
+						<div className='flex flex-col items-center gap-4'>
+							<LoaderCircle className='animate-spin'></LoaderCircle>
+							<p className='text-sm font-medium'>Loading Chats...</p>
+						</div>
 					</div>
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</>
 	);
 }
